b6ec792fd5869255e073cadf1fe02c65ffef45bb
Royston Monteiro <rpm319@nyu.edu>
2015-08-17 21:28:34 -0700

    ok
    ok

---
+++ project/scratch.R
@@ -0,0 +1,3 @@
+#training <- read.csv("T:/dev/ws/DataScience/PracticalMachineLearning/PredictingWithTrees/PracticalMachineLearning/PracticalMachineLearning-Scratch-Pad/project/pml-training.csv")
+#testing <- read.csv("T:/dev/ws/DataScience/PracticalMachineLearning/PredictingWithTrees/PracticalMachineLearning/PracticalMachineLearning-Scratch-Pad/project/pml-testing.csv")
+
